53a9f284526522f31d04b50dc73fcb0b0e6868d4
mauronofrio <poronofrio@gmail.com>
2018-12-28 17:44:01 +0100

    Added some others configs in device.mk

--- device.mk
+++ device.mk
@@ -1,12 +1,34 @@
 
 LOCAL_PATH := device/oneplus/fajita
 
-# Getting this to build in my local omni tree proved quite difficult so just using a prebuilt update_engine_sideload for now.
-# Oddly enough, OnePlus stock recovery does not include an update_engine_sideload binary either.
+# define hardware platform
+PRODUCT_PLATFORM := sdm845
+
+# A/B support
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier
+
+PRODUCT_PACKAGES += \
+    bootctrl.sdm845
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
 # Enable update engine sideloading by including the static version of the
 # boot_control HAL and its dependencies.
-#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-#    libsparse
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.sdm845 \
+    libgptutils \
+    libz \
+    libcutils
 
-#PRODUCT_PACKAGES += \
-#    update_engine_sideload
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
